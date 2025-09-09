be8b04c6416743e02a6de52777b83d4c744b80a9
Angelo Di Marco <adi-marc@c1r6s4.42luxembourg.lu>
2025-09-09 13:23:49 +0200

    fixed comments

--- src/philo_init.c
+++ src/philo_init.c
@@ -6,7 +6,7 @@
 /*   By: adi-marc <adi-marc@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 14:31:44 by adi-marc          #+#    #+#             */
-/*   Updated: 2025/09/09 11:47:11 by adi-marc         ###   ########.fr       */
+/*   Updated: 2025/09/09 13:22:18 by adi-marc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,71 +65,6 @@ int	ft_setup_mutexes(t_philo_env *env)
 	return (1);
 }
 
-/* Destroy mutexes and free allocated memory
-void	ft_cleanup(t_philo_env *env)
-{
-	int			i;
-	t_philo_env	*env;
-
-	if (!env)
-		return ;
-	if (env->forks)
-	{
-		i = 0;
-		while (i < env->count)
-		{
-			pthread_mutex_destroy(&env->forks[i]);
-			i++;
-		}
-		free(env->forks);
-	}
-	if (env->philos)
-	{
-		i = 0;
-		while (i < env->count)
-		{
-			if (env->philos[i])
-			{
-				pthread_mutex_destroy(&env->philos[i]->meal_lock);
-				free(env->philos[i]);
-			}
-			i++;
-		}
-		free(env->philos);
-	}
-	pthread_mutex_destroy(&env->write_lock);
-	pthread_mutex_destroy(&env->stop_lock);
-	free(env);
-}*/
-/* Parse arguments, validate them and set up the environment
-t_philo_env	*ft_init_env(int argc, char **argv)
-{
-	if (argc != 5 && argc != 6)
-		return (NULL);
-	env = ft_create_env(argc, argv);
-	if (!env)
-		return (NULL);
-	if (!ft_validate_env(env, argc))
-	{
-		free(env);
-		return (NULL);
-	}
-	env->stop = 0;
-	env->forks = NULL;
-	env->philos = NULL;
-	env->start_time = 0;
-	if (!ft_setup_mutexes(env))
-	{
-		ft_cleanup(env);
-		return (NULL);
-	}
-	if (!ft_setup_philos(env))
-	{
-		ft_cleanup(env);
-		return (NULL);
-	}
-	return (env);
-}*/
 /* Helper to init environment fields */
 static void	ft_init_env_fields(t_philo_env *env)
 {
--- src/philo_monitor.c
+++ src/philo_monitor.c
@@ -6,11 +6,10 @@
 /*   By: adi-marc <adi-marc@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 14:31:49 by adi-marc          #+#    #+#             */
-/*   Updated: 2025/09/09 10:37:24 by adi-marc         ###   ########.fr       */
+/*   Updated: 2025/09/09 13:22:30 by adi-marc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* Monitor the philosophers for death or full meals */
 #include "../includes/philo.h"
 
 /* Helper for the monitor thread; returns 1 if a philosopher dies */
@@ -34,38 +33,6 @@ int	ft_check_philo_status(t_philo_env *env, int i, int *full_count)
 	pthread_mutex_unlock(&env->philos[i]->meal_lock);
 	return (0);
 }
-
-/* Monitor thread to detect death and meal completion 
-void	*ft_monitor(void *arg)
-{
-	t_philo_env	*env;
-	int			i;
-	int			full_count;
-
-	env = (t_philo_env *)arg;
-	if (env->meals == 0)
-		return (NULL);
-	while (ft_current_time() < env->start_time)
-		usleep(50);
-	while (!ft_is_stopped(env))
-	{
-		i = 0;
-		full_count = 0;
-		while (i < env->count)
-		{
-			if (ft_check_philo_status(env, i, &full_count))
-				return (NULL);
-			i++;
-		}
-		if (env->meals > 0 && full_count == env->count)
-		{
-			ft_request_stop(env);
-			return (NULL);
-		}
-		usleep(500);
-	}
-	return (NULL);
-}*/
 
 static int	ft_monitor_cycle(t_philo_env *env)
 {
